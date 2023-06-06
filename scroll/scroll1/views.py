d2dabf5839f0a394e8f5d0ab87616303336da99d
Rafael Recoder <rrecoder@clt.com.py>
2023-06-06 16:07:59 -0400

    Interfaces de usuario y javascript

---
+++ scroll/scroll1/views.py
@@ -0,0 +1,22 @@
+import time
+from django.http import JsonResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'posts/index.html')
+
+def posts(request):
+    # Get start and end points
+    start = int(request.GET.get("start") or 0)
+    end = int(request.GET.get("end") or (start + 9))
+
+    # Generate list of post
+    data = []
+    for i in range(start, end + 1):
+        data.append(f"Post #{i}")
+
+    # Delay reponse
+    time.sleep(1)
+    
+    return JsonResponse({"posts": data})
